659c7cb388f42059ae8331074742ce1242ac2994
kana-chan <bkm0115_y@icloud.com>
2020-03-04 12:59:48 +0900

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "tweets#index"
-  resources :tweets
+
+  namespace :tweets do
+    resources :searches, only: [:index]
+  end
+
+  resources :tweets do
+    resources :comments, only: [:create]
+  end
+
   resources :users, only: [:show]
+
 end
